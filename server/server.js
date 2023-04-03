7a5e3b1ceef0cae2c15e7a5459fe8121aa784510
Winston Lee <wlee@ucsb.edu>
2023-04-03 15:33:39 -0700

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import * as dotenv from "dotenv";
 
 import snippetRoutes from "./routes/snippets.js";
 import folderRoutes from "./routes/folders.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -15,12 +18,23 @@ app.use(cors());
 app.use("/folders", folderRoutes);
 app.use("/snippets", snippetRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://Winston:wlee261@cluster0.0koaf.mongodb.net/snippet_app?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
